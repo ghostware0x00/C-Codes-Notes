739056cf6c1f807542854a4f31d408b63dbfc88c
ghostware0x00 <souravbose361@gmail.com>
2026-05-31 13:39:40 +0530

    file handing in c

--- Programs/File Handling/file_handle.c
+++ Programs/File Handling/file_handle.c
@@ -1,91 +1,100 @@
 #include<stdio.h>
-#include<stdlib.h>
-#define FAIL "\x1b[1;91m" // Failure messages (USE ANSII ESCAPE CODES) syntax => "\x1b[1;COLOR_CODEm" the 1; => to apply contrast bold effect
-#define SUCCESS "\x1b[1;92m"// success messages
-#define WARNING "\x1b[1;93m"// warning agesmess
 
-void elf_corrupt(FILE *fileptr){// function to display msg when elf binary file bytes are corrupted
-	printf(FAIL "[x]ELF file corrupted\n");
-	fclose(fileptr);
-	exit(0);
-}
+struct threeSum{
+	int n1, n2, n3, n4;
+};
 
-void arch_endian_version(unsigned char mag[], FILE *fileptr){ // check architecture, endiannes, version
-	if((mag[4] != 0x1 && mag[4] != 0x2) || (mag[5] != 0x1 && mag[5] != 0x2) || (mag[6] != 0x1))
-		elf_corrupt(fileptr);
-	if(mag[4] == 0x1)// architecture(class) 0x1 = 32bit 0x2 = 64bit
-		printf(SUCCESS "\tClass : ELF32\n");
-	else
-		printf(SUCCESS "\tClass : ELF64\n");
-	if(mag[5] == 0x1)// data 0x1 = little 0x2 = big
-		printf(SUCCESS "\tData : little endian\n");
-	else
-		printf(SUCCESS "\tData : big endian\n"); // mag[6]
-	printf(SUCCESS "\tVersion : 0x1\n");
-}
-
-void os_abis_version(unsigned char mag[], FILE *fileptr){// function to find os_abis, abis_version
-	//byte 7 -15 is os, abi, abi_version
-	// based on the value the abi follows that os abi rules
-	// os_abis indicate what operating system abi rules does this particular abi follow. so if 0x3 = Linux then, the abi follows linux abi rules
-	if(mag[7] != 0x0 && mag[7] != 0x1 && mag[7] != 0x2 && mag[7] !=0x3 && mag[7] != 0x6 && mag[7] != 0x9)
-		elf_corrupt(fileptr);
-	if(mag[7] == 0x0)
-		printf(SUCCESS "\tOS/ABI : UNIX - SYSTEM V\n");
-	else if(mag[7] == 0x1)
-		printf(SUCCESS "\tOS/ABI : HP-UX\n");
-	else if(mag[7] == 0x2)
-		printf(SUCCESS "\tOS/ABI : NetBSD\n");
-	else if(mag[7] == 0x3)
-		printf(SUCCESS "\tOS/ABI : Linux\n");
-	else if(mag[7] == 0x6)
-		printf(SUCCESS "\tOS/ABI : Solaris\n");
-	else if(mag[7] == 0x9)
-		printf(SUCCESS "\tOS/ABI : FreeBSD\n");
-	printf(SUCCESS "\tABI Version : %d\n",mag[8]);// abi version
-}
-
-void elfHeader(char *filename){// here program accepts only 1 string and char * means the pointer starts from the first character of the string until it hits null terminator
-	FILE *fileptr;
-	fileptr = fopen(filename, "rb"); // reading binary file
-	if(fileptr == NULL){
-		perror(FAIL "[-]file open failed.\n");
-		exit(0);
+int main(){
+	// to create a file open a file, read a file or write a file we need to declare a pointer named -> *FILE 
+	// to open the file we use the fopen
+	FILE *fileptr; // creating the file pointer
+	fileptr = fopen("elf2", "w"); // "w" mode is to write inside a file. if the file doesnt exist the program will create it.
+									// if you want to create a file to a specific folder just provide the absolute path.
+									// problem with "w" mode is that it overwrites whatever content is inside the file with the new stuff you write using fprintf
+	fprintf(fileptr, "lolbruh");// writing content inside the file.
+	fclose(fileptr);// closing the file pointer is necessary
+	FILE *fileptr2;
+	fileptr2 = fopen("elf2", "a"); // opening file in "a" append mode 
+									// this allows us to write inside the file without overwriting the content already present there.
+	fprintf(fileptr2, "\nwhassup2\n");
+	fclose(fileptr2);
+	FILE *fileptr3;
+	fileptr3 = fopen("elf2", "r");
+	if(fileptr3 == NULL){	// good practice to check whether we were able to open file or not.
+		printf("not able to open file");
 	}
-	unsigned char mag[18];// 7 chars = 7 bytes for storing first 4 bytes(magic bytes) and the rest 3 bytes for architecture, endianness, version. unsigned to avoid sign bit interpretations
-	fread(&mag, 1, sizeof(mag), fileptr); // reads total 4 bytes (1 byte each). args2 = 1 byte args3 = sizeof(mag) which is 4 so 1 x 4 = 4 bytes total
-	// [7F 45 4C 46] magic bytes of an elf binary
-	unsigned char magic_bytes[] = {0x7f, 0x45, 0x4c, 0x46}; // unsigned to avoid sign bit interpretations
+	char read_data[100];// 100 characters so 1 char = 1 byte hence 100 characters = 100 bytes.
+	printf("fileptr3 reading content ----->\n");
+	fgets(read_data, 100, fileptr3);	// fgets is used to read contents from a input or from a file.
+	// better than gets() which causes buffer overflow because fgets prevents buffer overflow by specifying the max number of characters to read
+	// problem with reading files like this is that if newline is encountered \n then only the first line will be stored.
+	// fgets (<variable where file_content to store>, <max number of characters to read>, <file pointer variable>)
+	printf("%s", read_data);
+	fclose(fileptr3);
+	FILE *fileptr4;
+	fileptr4 = fopen("elf2", "r");
+	if(fileptr3 == NULL){	// good practice to check whether we were able to open file or not.
+		printf("not able to open file");
+	}
+	char read_data2[100];
+	printf("fileptr4 reading content ----->\n");
+	while(fgets(read_data2, 100, fileptr4)){ // reading files like these stores the file contents including new line contents.
+		printf("%s", read_data2);
+	}
+
+	// check the geeksfogeeks for binary file handling in c [https://www.geeksforgeeks.org/c/basics-file-handling-c/]
+	FILE *fileBinaryWrite;
+	struct threeSum sum;
+	fileBinaryWrite = fopen("hello.bin", "wb"); // "wb" means write binary file. 
+	sum.n1 = 10;
+	sum.n2 = 20;
+	sum.n3 = 30;
+	sum.n4 = sum.n1 + sum.n2 + sum.n3;
+	int flag = 0;
+	flag = fwrite(&sum, sizeof(struct threeSum), 1, fileBinaryWrite);
+	// writing to a binary file using fwrite()
+	// fwrite args -> fwrite(<pointer to data>, <size of one element>, <number of elements>, <file pointer>)
 	/*
-	so raw byte is in 0s and 1s
-	compiler sees hex value 0x
-	converts it to its corresponding 0s and 1s
-	compares the fread raw each bytes with the 0s and 1s of the hex value
+	args1 requires the address of the memory block whose data will be written to the binary file.
+	args2 specifies the size of each data element in bytes.
+	args3 specifies how many such elements to write.
+	args4 is the file pointer representing the binary file stream.
 	*/
-	for(int i=0; i<4; i++){
-		if(mag[i] != magic_bytes[i]){
-			printf(FAIL "[x]Not ELF Binary\n");
-			fclose(fileptr);
-			exit(0);
-		}
+	// so basically in the program above in fwrite
+	// i am writing 16 bytes(cuz 4 integers in struct) to flag variable
+	// since there is only 1 instance of struct i am writing so 1.
+	//if i wanted to write multiple instances so i can specify the number accordingly. So if 3 instances total size written would be 16 x 3 = 48 bytes to flag variable.
+	// the flag variable is basically for status checking whether binary write succesfully happened or not. if flag = 1 then ok else not ok.
+	printf("binary file write -------->\n");
+	if(flag){// if fwrite works then flag value will change to 1
+		printf("Write operation was successful\n");
 	}
-	printf(SUCCESS "[*]ELF Binary\n");
-	printf(SUCCESS "ELF Header :\n");
-	arch_endian_version(mag, fileptr);
-	os_abis_version(mag, fileptr);
-	fclose(fileptr);
-	return;
-}
-
-int main(int argc, char *argv[]){
-	// c doesnt have a string variable
-	// so we use (char *) to point to the first character of the string in memory
-	// [] used to store multiple arguments. Without [] if there is any space for like different args so it will only store the first arg until space encountered
-	if(argc < 2 || argc > 2){
-		printf(WARNING "[-]incorrect/missing arguments");
-		exit(0);
+	else{
+		printf("Write operation was unsuccessful\n");
 	}
-	elfHeader(argv[1]);
-	// isStatic_Dynamic_Linked(argv[1]);
+	fclose(fileBinaryWrite);
+	FILE *fileBinaryRead;
+	fileBinaryRead = fopen("hello", "rb"); // "rb" means read binary "hello"
+	//fread(<memory destination>, <size of each element>, <number of elements>, <file pointer>);
+	/*
+	args1 requires the address of the memory block where the binary data(bytes) of the binary file will  be stored.
+	args2 specifies the size of the data to read
+	args3 specifies the number of elements of that same size to read
+	args4 specifies the binary file pointer variable 
+	*/
+	fread(&sum, sizeof(struct threeSum), 1, fileBinaryRead);
+	// here struct is 16 bytes so fread will read the first 16 bytes from the "hello" into the struct threeSum
+	// the data will be stored inside the struct data members n1, n2, n3, n4
+	printf("Reading binary file and storing binary data in struct ------>\n");
+	printf("n1 = 0x%x\tn2 = 0x%x\tn3 = 0x%x\tn4 = 0x%x\n",sum.n1, sum.n2, sum.n3, sum.n4);// using struct variable displaying the data
+	// data stored 4 byte per variable of struct cuz int = 4 bytes and total data to read = 16 bytes
+	rewind(fileBinaryRead);
+	unsigned char store[16]; // unsigned char is used because it preserves raw byte values exactly from 0x00 to 0xFF (0–255) without signed interpretation issues.
+	fread(&store, sizeof(store), 1, fileBinaryRead);
+	printf("Reading binary file and storing binary data in character array------>\n");
+	for(int i=0; i<16; i++){
+		printf("0x%x\n",store[i]);
+	}
+	fclose(fileBinaryRead);
 	return 0;
 }
